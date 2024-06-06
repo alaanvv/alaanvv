e7153b5a8071929dd5e0eae4dca29e0f8842fef0
Alan Vale <valealan84@gmail.com>
2024-06-05 22:30:31 -0400

    New dotfiles

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,88 @@
+local ascii = {
+  neovim = {
+    [[                               __                ]],
+    [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+    [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+    [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+    [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+    [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  },
+
+  bonfire = {
+    '⠀⠀⠀⠀⠀⠐⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠈⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⣈⣼⣄⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠉⠑⢷⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⣼⣐⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠘⡚⢧⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠀⢃⢿⡇⠀⠀⡾⡀⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠸⣇⠀⠀⠡⣰⠀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⣿⠀⢠⣄⢿⠇⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⢸⡇⠜⣭⢸⡀⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⡙⣿⣿⠰⢫⠁⣇⠀⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⢰⣽⠱⡈⠋⠋⣤⡤⠳⠉⡆⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠀⠀⡜⠡⠊⠑⠄⣠⣿⠃⠀⣣⠃⠀⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⠀⠐⣼⡠⠥⠊⡂⣼⢀⣤⠠⡲⢂⡌⡄⠀⠀⠀⠀⠀',
+    '⠀⠀⠀⠀⣀⠝⡛⢁⡴⢉⠗⠛⢰⣶⣯⢠⠺⠀⠈⢥⠰⡀⠀⠀',
+    '⠀⣠⣴⢿⣿⡟⠷⠶⣶⣵⣲⡀⣨⣿⣆⡬⠖⢛⣶⣼⡗⠈⠢⠀',
+    '⢰⣹⠭⠽⢧⠅⢂⣳⠛⢿⡽⣿⢿⡿⢟⣟⡻⢾⣿⣿⡤⢴⣶⡃',
+  },
+
+  hydra = {
+    '                                   ',
+    '   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ',
+    '    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ',
+    '          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ',
+    '           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ',
+    '          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ',
+    '   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ',
+    '  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ',
+    ' ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ',
+    ' ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ',
+    '      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ',
+    '       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ',  
+  }
+}
+
+quotes = {
+  soulsborne = {
+    { 'The brighter a flame, the deeper the shadow', '', '- Vendrick' },
+    { 'Every age it seems, is tainted by the greed of men', '', '- Patches' },
+    { 'Like a moth drawn to the flame, your wings will burn in anguish, time after time', '', '- Shrine Handmaid' },
+    { 'Men are props on the stage of life, and no matter how tender, how exquisite... a lie will remain a lie!', '', '- Aldia' },
+    { 'The world began without knowledge and without knowledge will it end' },
+    { 'Seek strength... the rest will follow', '', '- Vendrick' },
+    { 'What is bravery without a bit of recklessness?', '', 'Hawkeye Gough' },
+    { 'For the curse of life is the curse of want. And so, you peer into the fog, in hope of answers' },
+    { 'Only in truth, the Lords will abandon their thrones, and the Unkindled will rise. Nameless accursed undead, unfit even to be cinder. And so it is that ash seeketh embers' },
+    { 'Fear not the dark, my friend. And let the feast begin!' },
+    { 'There is no path. Beyond the scope of light, beyond the reach of Dark. What could possibly await us? And yet, we seek it, insatiably... such is our fate' },
+    { 'You\'ve got a heart of gold. Don\'t let them take it from you' }
+  }
+}
+
+return {	
+  'goolord/alpha-nvim',
+
+  config = function()
+    local alpha     = require('alpha')
+    local dashboard = require('alpha.themes.dashboard')
+
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "     browse",      ":Telescope find_files <CR>"),
+      dashboard.button("r", "     recents",     ":Telescope oldfiles <CR>"),
+      dashboard.button("e", "     new-file",    ":ene <BAR> startinsert <CR>"),
+      dashboard.button("c", "     colorscheme", ":Telescope colorscheme <CR>"),
+      dashboard.button("s", "     settings",    ":e $MYVIMRC <CR>"),
+      dashboard.button("q", "     quit",        ":qa <CR>"),
+    }
+
+    dashboard.section.header.val = ascii.hydra
+    -- dashboard.section.footer.val = require'alpha.fortune'({ fortune_list = nil })
+
+    alpha.setup(dashboard.opts)
+
+    vim.api.nvim_set_keymap('n', '<leader>a', ':Alpha <CR>', {})
+  end
+}
