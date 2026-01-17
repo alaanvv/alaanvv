fa51f49661df6f27a17444d4f9364856dbb801be
alaanvv <valealan84@gmail.com>
2026-01-17 08:15:32 -0300

    svelte in neovim and cfzf
    cfzf is a command to fuzzy find my public repos for cloning

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -22,23 +22,6 @@ vim.cmd('autocmd BufNewFile,BufRead *.[c|h] set filetype=cpp')
 vim.cmd('autocmd BufNewFile,BufRead *.[f|v] set filetype=')
 vim.cmd('autocmd VimLeavePre * mksession! ' .. vim.fn.stdpath('config') .. '/session.vim')
 
-vim.api.nvim_create_autocmd('TermOpen', {
-  pattern = "*",
-  callback = function()
-    local ti = 0
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-      if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
-        ti = ti + 1
-      end
-    end
-    ti = ti - 1
-
-    vim.api.nvim_buf_set_name(0, ti == 0 and 'terminal' or ('terminal[' .. ti .. ']'))
-    vim.opt_local.relativenumber = false
-    vim.opt_local.number         = false
-  end
-})
-
 for _, file in ipairs(vim.fn.glob(vim.fn.stdpath('config') .. '/lua/autorun/*.lua', false, true)) do
   if file:match('%.lua$') then
     require(file:match('.*/(autorun/.+)%.lua$'))
--- config/nvim/lua/plugins/svelte.lua
+++ config/nvim/lua/plugins/svelte.lua
@@ -1,5 +1,3 @@
 return {
-  'othree/html5.vim',
-  'pangloss/vim-javascript',
-  'evanleck/vim-svelte'
+  'leafOfTree/vim-svelte-plugin'
 }
