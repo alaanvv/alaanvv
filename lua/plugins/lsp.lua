abc8656b8b05927ddc0dcf1d92991cf96bc47eab
alaanvv <valealan84@gmail.com>
2026-02-06 20:40:12 -0300

    feat: lsp update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,49 +1,22 @@
-vim.keymap.set('i', '<C-g>', '<C-x><C-o>')
-vim.keymap.set('i', '<C-j>', 'pumvisible() ? \'<C-n>\' : \'<C-n>\'', { expr = true })
-vim.keymap.set('i', '<C-k>', 'pumvisible() ? \'<C-p>\' : \'<C-p>\'', { expr = true })
-vim.keymap.set('i', '<C-h>', 'pumvisible() ? \'<C-e>\' : \'<C-h>\'', { expr = true })
-vim.keymap.set('i', '<C-l>', 'pumvisible() ? \'<C-y>\' : \'<C-l>\'', { expr = true })
-vim.keymap.set('n', '<leader>E', ':lua vim.diagnostic.goto_next() <CR>')
-vim.keymap.set('n', '<C-p>', vim.lsp.buf.hover)
-vim.keymap.set('n', '<A-f>', vim.lsp.buf.format)
-vim.keymap.set('n', '<C-c>', vim.lsp.buf.rename)
+vim.keymap.set('n', '<leader>ac', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<leader>E', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<C-f>', vim.lsp.buf.references)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-vim.keymap.set('n', '<leader>ac', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<A-f>', vim.lsp.buf.format)
+vim.keymap.set('n', '<C-c>', vim.lsp.buf.rename)
+vim.keymap.set('n', '<C-p>', vim.lsp.buf.hover)
+vim.keymap.set('i', '<C-g>', '<C-x><C-o>')
 
-vim.api.nvim_create_autocmd('CursorHold', {
-  desc = 'Show diagnostics on hover',
-  pattern = '*',
-  callback = function()
-    if #vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 }) == 0 then return end
-    vim.diagnostic.open_float(nil, { close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' } })
-  end
-})
-
-vim.diagnostic.config({
-  float = {
-    header = '',
-    prefix = '',
-    border = 'solid',
-    source = false,
-    focusable = false
-  }
-})
+vim.diagnostic.config({ virtual_text = true })
 
 return {
+  'neovim/nvim-lspconfig',
   {
     'williamboman/mason.nvim',
-    config = function()
-      require('mason').setup()
-    end
+    config = function() require('mason').setup() end
   },
-
   {
     'williamboman/mason-lspconfig.nvim',
-    config = function()
-      require('mason-lspconfig').setup()
-    end
+    config = function() require('mason-lspconfig').setup() end
   },
-
-  'neovim/nvim-lspconfig'
 }
