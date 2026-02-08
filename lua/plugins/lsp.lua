93a3de2e50fe0dcaf1bd605c8cf6638ded19ce0d
alaanvv <valealan84@gmail.com>
2026-02-08 09:07:43 -0300

    refactor: removing existent keybind
    i can just use cxco

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,6 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', '<A-f>', vim.lsp.buf.format)
 vim.keymap.set('n', '<C-c>', vim.lsp.buf.rename)
 vim.keymap.set('n', '<C-p>', vim.lsp.buf.hover)
-vim.keymap.set('i', '<C-g>', '<C-x><C-o>')
 
 vim.diagnostic.config({ virtual_text = true })
 
