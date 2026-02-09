ef1dfb28dbeb95fac53555cc18ce3826fecd7176
alaanvv <valealan84@gmail.com>
2026-02-09 19:34:34 -0300

    refactor: removing unused diagnostic disable
    idk why it was here i waasnt supressing shit

--- lua/autorun/diff.lua
+++ lua/autorun/diff.lua
@@ -1,6 +1,5 @@
 if not vim.wo.diff then return end
 
----@diagnostic disable-next-line: undefined-field
 vim.opt.fillchars:append({ diff = ' ', fold = ' ' })
 vim.o.diffopt = 'internal,filler,closeoff,linematch:40,foldcolumn:0'
 vim.o.number = false
