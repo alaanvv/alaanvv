358de27c53a1539663a9679f6a99e2631d7a31fd
alaanvv <valealan84@gmail.com>
2026-02-12 23:06:03 -0300

    feat: removing hjkl movement in insert and jj for quit cuz i discovered <C-c>

--- lua/autorun/keymap.lua
+++ lua/autorun/keymap.lua
@@ -1,18 +1,11 @@
 -- Basic Vim Moves
 vim.keymap.set('n', 'x', '"_x')
 vim.keymap.set('v', 'p', '"_dP')
-vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', ';', ':')
 vim.keymap.set('v', ';', ':')
 
--- HJKL movement in insert mode
-vim.keymap.set('i', '<C-h>', '<Left>')
-vim.keymap.set('i', '<C-j>', '<Down>')
-vim.keymap.set('i', '<C-k>', '<Up>')
-vim.keymap.set('i', '<C-l>', '<Right>')
-
 -- Line operations
-vim.keymap.set('n', 'J', ':t.        <CR>')
-vim.keymap.set('n', 'K', ':t.-1      <CR>')
-vim.keymap.set('n', '<A-k>', ':m .-2 <CR>')
-vim.keymap.set('n', '<A-j>', ':m .+1 <CR>')
+vim.keymap.set('n', 'J',     ':t.  <CR>')
+vim.keymap.set('n', 'K',     ':t-1 <CR>')
+vim.keymap.set('n', '<A-k>', ':m-2 <CR>')
+vim.keymap.set('n', '<A-j>', ':m+1 <CR>')
