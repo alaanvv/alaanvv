2d80c61822e7a1522ba2fd6dff2e4de08584a96e
alaanvv <valealan84@gmail.com>
2026-06-08 16:07:29 -0300

    feat: treesitter update
    honestly i dont know what it does

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,6 @@
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
-vim.opt.foldenable = true
-vim.opt.foldlevel  = 99
-
 return {
   'nvim-treesitter/nvim-treesitter',
-  branch = 'master',
+  branch = 'main',
   lazy = false,
   build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
-  end
 }
